2263582835012c25f28393e167e9183502ff5122
Roman Juszczyk <roman.juszczyk@zoxs.de>
2025-02-28 16:33:52 +0100

    work

--- home/programs/astronvim/template/lua/community.lua
+++ home/programs/astronvim/template/lua/community.lua
@@ -16,7 +16,56 @@ return {
       }
     },
   },
-  { import = "astrocommunity.pack.laravel" },
+
   { import = "astrocommunity.pack.nix" },
+  { import = "astrocommunity.pack.vue" },
   -- import/override with your plugins folder
+  { import = "astrocommunity.pack.laravel" },
+  -- Override phpactor with intelephense and php-cs-fixer with pint.
+  {
+    "williamboman/mason-lspconfig.nvim",
+    opts = function(_, opts)
+      -- Replace phpactor with intelephense by first filtering out phpactor and then adding intelephense
+      opts.ensure_installed = vim.tbl_filter(function(item) 
+        return item ~= "phpactor" 
+      end, opts.ensure_installed)
+      opts.ensure_installed = require("astrocore").list_insert_unique(opts.ensure_installed, { "intelephense" })
+    end,
+  },
+  {
+    "jay-babu/mason-null-ls.nvim",
+    optional = true,
+    opts = function(_, opts)
+      -- Remove php-cs-fixer by filtering
+      opts.ensure_installed = vim.tbl_filter(function(item) 
+        return item ~= "php-cs-fixer" 
+      end, opts.ensure_installed)
+      -- Add pint
+      opts.ensure_installed = require("astrocore").list_insert_unique(opts.ensure_installed, { "pint" })
+    end,
+  },
+  {
+    "WhoIsSethDaniel/mason-tool-installer.nvim",
+    optional = true,
+    opts = function(_, opts)
+      -- Remove phpactor and php-cs-fixer by filtering
+      opts.ensure_installed = vim.tbl_filter(function(item) 
+        return item ~= "phpactor" and item ~= "php-cs-fixer"
+      end, opts.ensure_installed)
+      -- Add intelephense and pint
+      opts.ensure_installed = require("astrocore").list_insert_unique(
+        opts.ensure_installed,
+        { "intelephense", "php-debug-adapter", "pint" }
+      )
+    end,
+  },
+  {
+    "stevearc/conform.nvim",
+    optional = true,
+    opts = {
+      formatters_by_ft = {
+        php = { "pint" },
+      },
+    },
+  },
 }
--- home/programs/git/default.nix
+++ home/programs/git/default.nix
@@ -1,6 +1,4 @@
-{ pkgs, ... }:
-
-let
+{pkgs, isWork, ...}: let
   gitConfig = {
     core = {
       editor = "nvim";
@@ -28,8 +26,7 @@ let
   };
 
   rg = "${pkgs.ripgrep}/bin/rg";
-in
-{
+in {
   home.packages = with pkgs.gitAndTools; [
     diff-so-fancy # git diff with colors
     git-crypt # git files encryption
@@ -37,42 +34,54 @@ in
     tig # diff and commit view
   ];
 
-  programs.git = {
-    enable = true;
-    aliases = {
-      amend = "commit --amend -m";
-      fixup = "!f(){ git reset --soft HEAD~\${1} && git commit --amend -C HEAD; };f";
-      loc = "!f(){ git ls-files | ${rg} \"\\.\${1}\" | xargs wc -l; };f"; # lines of code
-      br = "branch";
-      co = "checkout";
-      cob = "checkout -b";
-      st = "status";
-      ls = "log --pretty=format:\"%C(yellow)%h%Cred%d\\\\ %Creset%s%Cblue\\\\ [%cn]\" --decorate";
-      ll = "log --pretty=format:\"%C(yellow)%h%Cred%d\\\\ %Creset%s%Cblue\\\\ [%cn]\" --decorate --numstat";
-      cm = "commit -m";
-      ca = "commit -am";
-      dc = "diff --cached";
-      rmain = "rebase main";
-      rc = "rebase --continue";
-    };
-    extraConfig = gitConfig;
-    ignores = [
-      "*.bloop"
-      "*.bsp"
-      "*.metals"
-      "*.metals.sbt"
-      "*metals.sbt"
-      "*.direnv"
-      "*.envrc" # there is lorri, nix-direnv & simple direnv; let people decide
-      "*hie.yaml" # ghcide files
-      "*.mill-version" # used by metals
-      "*.jvmopts" # should be local to every project
-    ];
-    signing = {
-      key = "7AE6055A1268DAD6";
-      signByDefault = true;
-    };
-    userEmail = "contact@romek.codes";
-    userName = "romek";
-  } // (pkgs.sxm.git or { });
+  programs.git =
+    {
+      enable = true;
+      aliases = {
+        amend = "commit --amend -m";
+        fixup = "!f(){ git reset --soft HEAD~\${1} && git commit --amend -C HEAD; };f";
+        loc = "!f(){ git ls-files | ${rg} \"\\.\${1}\" | xargs wc -l; };f"; # lines of code
+        br = "branch";
+        co = "checkout";
+        cob = "checkout -b";
+        st = "status";
+        ls = "log --pretty=format:\"%C(yellow)%h%Cred%d\\\\ %Creset%s%Cblue\\\\ [%cn]\" --decorate";
+        ll = "log --pretty=format:\"%C(yellow)%h%Cred%d\\\\ %Creset%s%Cblue\\\\ [%cn]\" --decorate --numstat";
+        cm = "commit -m";
+        ca = "commit -am";
+        dc = "diff --cached";
+        rmain = "rebase main";
+        rc = "rebase --continue";
+      };
+      extraConfig = gitConfig;
+      ignores = [
+        "*.bloop"
+        "*.bsp"
+        "*.metals"
+        "*.metals.sbt"
+        "*metals.sbt"
+        "*.direnv"
+        "*.envrc" # there is lorri, nix-direnv & simple direnv; let people decide
+        "*hie.yaml" # ghcide files
+        "*.mill-version" # used by metals
+        "*.jvmopts" # should be local to every project
+      ];
+      signing = {
+        key =
+          if isWork
+          then "86FB80FCC9361C0C"
+          else "7AE6055A1268DAD6";
+        signByDefault = true;
+      };
+
+      userEmail =
+        if isWork
+        then "roman.juszczyk@zoxs.de"
+        else "contact@romek.codes";
+      userName =
+        if isWork
+        then "Roman Juszczyk"
+        else "romek";
+    }
+    // (pkgs.sxm.git or {});
 }
