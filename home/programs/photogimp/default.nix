5e9e4e06623301b0709d20d6d866f44a944d3b93
romek <contact@romek.codes>
2025-04-06 16:11:52 +0200

    pin photogimp to 2.10 as long as 3.0 is not yet in nixpkgs

--- home/programs/photogimp/default.nix
+++ home/programs/photogimp/default.nix
@@ -5,9 +5,9 @@
       pkgs.fetchFromGitHub {
         owner = "Diolinux";
         repo = "PhotoGIMP";
-        rev = "master";
-        sha256 = "sha256-tWbkHf0Es/I0oLooep+GIFhFXWgGxanBUUCpzHMzC1I=";
+        rev = "photogimp-2.10"; # update revision to master when gimp 3.0 is in nixpkgs
+        sha256 = "sha256-OLEqtI2Hem2fTTL0KNf0aZsFfuwwhgE4etyRMcW5KiQ=";
       }
-      + "/.var/app/org.gimp.GIMP/config/GIMP/2.10";
+      + "/.var/app/org.gimp.GIMP/config/GIMP/2.10"; # update path when gimp 3.0 is in nixpkgs
   };
 }
