c840e88709455beb38bf4103ccae8c238ca5995e
romek <contact@romek.codes>
2025-05-02 23:30:03 +0200

    firefox addons etc.

--- home/programs/firefox/default.nix
+++ home/programs/firefox/default.nix
@@ -7,28 +7,30 @@
 }: let
   inherit (specialArgs) addons;
 
-  customAddons = pkgs.callPackage ./addons.nix {
-    inherit lib;
-    inherit (specialArgs) buildFirefoxXpiAddon;
-  };
+  # In case i wanna use a custom addon imma keep it here
+  # customAddons = pkgs.callPackage ./addons.nix {
+  #   inherit lib;
+  #   inherit (specialArgs) buildFirefoxXpiAddon;
+  # };
 
+  # TODO:
+  # Installing this addon requires the following additional steps:
+  # - Installing firenvim as a neovim plugin (see https://github.com/glacambre/firenvim)
+  # - Running the `:call firenvim#install(0)` command.
   extensions = with addons;
     [
-      bitwarden
       darkreader
-      ff2mpv
-      # auto-accepts cookies, use only with privacy-badger & ublock-origin
-      istilldontcareaboutcookies
-      # languagetool
-      link-cleaner
-      privacy-badger
-      simple-tab-groups
-      to-deepl
       ublock-origin
-      unpaywall
-      vimium
-    ]
-    ++ (with customAddons; [old-github-feed]);
+      vimium-c
+      firefox-color
+      firefox-translations
+      decentraleyes
+      sidebery
+      vue-js-devtools
+      firenvim
+      onetab
+    ];
+    # ++ (with customAddons; [old-github-feed]);
 
   # disable the annoying floating icon with camera and mic when on a call
   disableWebRtcIndicator = ''
@@ -104,6 +106,8 @@
       "print.print_headerright" = "";
 
       "privacy.donottrackheader.enabled" = true;
+      # Removed support.mozilla.org and addons.mozilla.org, because no dark mode is supported and dark reader gets automatically disabled. IMPORTANT: Make sure you only use addons you trust! (Which mostly are none, but there's some one can't live without)
+      "extensions.webextensions.restrictedDomains" = "accounts-static.cdn.mozilla.net,accounts.firefox.com,addons.cdn.mozilla.net,api.accounts.firefox.com,content.cdn.mozilla.net,discovery.addons.mozilla.org,install.mozilla.org,oauth.accounts.firefox.com,profile.accounts.firefox.com,sync.services.mozilla.com";
 
       # Yubikey
       "security.webauth.u2f" = true;
@@ -115,75 +119,6 @@
     }
     // dpiSettings;
 
-  demoSettings =
-    {
-      "accessibility.force_disabled" = 1;
-      # disable Studies
-      # disable Normandy/Shield [FF60+]
-      # Shield is a telemetry system that can push and test "recipes"
-      "app.normandy.api_url" = "";
-      "app.normandy.enabled" = false;
-
-      "browser.aboutConfig.showWarning" = false;
-
-      # personalized Extension Recommendations in about:addons and AMO [FF65+]
-      # https://support.mozilla.org/kb/personalized-extension-recommendations
-      "browser.discovery.enabled" = false;
-      "browser.helperApps.deleteTempFileOnExit" = true;
-
-      "browser.newtabpage.activity-stream.default.sites" = "";
-      "browser.newtabpage.activity-stream.feeds.topsites" = false;
-      "browser.newtabpage.activity-stream.showSponsored" = false;
-      "browser.newtabpage.activity-stream.showSponsoredTopSites" = false;
-      "browser.uitour.enabled" = false;
-
-      # use Mozilla geolocation service instead of Google.
-      #"geo.provider.network.url"= "https://location.services.mozilla.com/v1/geolocate?key=%MOZILLA_API_KEY%";
-      # disable using the OS's geolocation service
-      "geo.provider.use_gpsd" = false;
-      "geo.provider.use_geoclue" = false;
-
-      # HIDDEN PREF: disable recommendation pane in about:addons (uses Google Analytics)
-      "extensions.getAddons.showPane" = false;
-      # recommendations in about:addons' Extensions and Themes panes [FF68+]
-      "extensions.htmlaboutaddons.recommendations.enabled" = false;
-
-      # disable Network Connectivity checks
-      # [1] https://bugzilla.mozilla.org/1460537
-      "network.connectivity-service.enabled" = false;
-
-      # integrated calculator
-      "browser.urlbar.suggest.calculator" = true;
-
-      # TELEMETRY
-
-      # disable new data submission
-      "datareporting.policy.dataSubmissionEnabled" = false;
-      # disable Health Reports
-      "datareporting.healthreport.uploadEnabled" = false;
-      # 0332: disable telemetry
-      "toolkit.telemetry.unified" = false;
-      "toolkit.telemetry.enabled" = false;
-      "toolkit.telemetry.server" = "data:,";
-      "toolkit.telemetry.archive.enabled" = false;
-      "toolkit.telemetry.newProfilePing.enabled" = false;
-      "toolkit.telemetry.shutdownPingSender.enabled" = false;
-      "toolkit.telemetry.updatePing.enabled" = false;
-      "toolkit.telemetry.bhrPing.enabled" = false;
-      "toolkit.telemetry.firstShutdownPing.enabled" = false;
-      # disable Telemetry Coverage
-      "toolkit.telemetry.coverage.opt-out" = true; # [HIDDEN PREF]
-      "toolkit.coverage.opt-out" = true; # [FF64+] [HIDDEN PREF]
-      "toolkit.coverage.endpoint.base" = "";
-      # disable PingCentre telemetry (used in several System Add-ons) [FF57+]
-      "browser.ping-centre.telemetry" = false;
-      # disable Firefox Home (Activity Stream) telemetry
-      "browser.newtabpage.activity-stream.feeds.telemetry" = false;
-      "browser.newtabpage.activity-stream.telemetry" = false;
-      "toolkit.telemetry.reportingpolicy.firstRun" = false;
-      "toolkit.telemetry.shutdownPingSender.enabledFirstsession" = false;
-      "browser.vpn_promo.enabled" = false;
-    };
 in {
   # For webdev & testing
   programs.chromium.enable = true;
